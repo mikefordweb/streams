5190d62be5129b95db019ec02838e10bad65b143
Mike Ford <mike@mikefordweb.com>
2019-05-18 09:29:41 -0500

    Add files via upload

---
+++ streams-no-modules/client/src/history.js
@@ -0,0 +1,7 @@
+// the history package was installed automatically with 
+// 'react-router-dom', as a dependency of 'react-router-dom'
+// below we are calling createHistory, a function we can call 
+// to create a new history object
+import createHistory from 'history/createBrowserHistory';
+
+export default createHistory();
